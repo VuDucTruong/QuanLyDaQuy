2f696d01be11966f54766ebde6c66a7c8a5ef2f1
HuynhDangQuang <quangptnk@gmail.com>
2023-05-23 15:54:53 +0700

    Thêm phiếu dịch vụ

--- QuanLyDaQuy/QuanLyDaQuy/Phieu/PhieuDichVu.Designer.cs
+++ QuanLyDaQuy/QuanLyDaQuy/Phieu/PhieuDichVu.Designer.cs
@@ -28,12 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_SoPhieu = new System.Windows.Forms.TextBox();
+            this.label_SoPhieu = new System.Windows.Forms.Label();
+            this.label_NgayLap = new System.Windows.Forms.Label();
+            this.label_KhachHang = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_ThemDV = new System.Windows.Forms.Button();
+            this.button_SuaDV = new System.Windows.Forms.Button();
+            this.button_XoaDV = new System.Windows.Forms.Button();
+            this.button_Huy = new System.Windows.Forms.Button();
+            this.button_OK = new System.Windows.Forms.Button();
+            this.label_CT_PhieuDV = new System.Windows.Forms.Label();
+            this.textBox_NgayLap = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 6);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 162);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.tableLayoutPanel1.SetRowSpan(this.dataGridView1, 4);
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(596, 168);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Controls.Add(this.label_KhachHang, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_SoPhieu, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label_SoPhieu, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label_CT_PhieuDV, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label_NgayLap, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_NgayLap, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button_ThemDV, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button_SuaDV, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button_XoaDV, 5, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.05263F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.94737F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 92F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(600, 332);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 6);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(596, 58);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Danh sách phiếu dịch vụ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox_SoPhieu
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox_SoPhieu, 2);
+            this.textBox_SoPhieu.Location = new System.Drawing.Point(103, 61);
+            this.textBox_SoPhieu.Name = "textBox_SoPhieu";
+            this.textBox_SoPhieu.Size = new System.Drawing.Size(194, 20);
+            this.textBox_SoPhieu.TabIndex = 5;
+            // 
+            // label_SoPhieu
+            // 
+            this.label_SoPhieu.AutoSize = true;
+            this.label_SoPhieu.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_SoPhieu.Location = new System.Drawing.Point(3, 58);
+            this.label_SoPhieu.Name = "label_SoPhieu";
+            this.label_SoPhieu.Size = new System.Drawing.Size(66, 18);
+            this.label_SoPhieu.TabIndex = 6;
+            this.label_SoPhieu.Text = "Số phiếu";
+            // 
+            // label_NgayLap
+            // 
+            this.label_NgayLap.AutoSize = true;
+            this.label_NgayLap.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_NgayLap.Location = new System.Drawing.Point(303, 58);
+            this.label_NgayLap.Name = "label_NgayLap";
+            this.label_NgayLap.Size = new System.Drawing.Size(65, 18);
+            this.label_NgayLap.TabIndex = 7;
+            this.label_NgayLap.Text = "Ngày lập";
+            // 
+            // label_KhachHang
+            // 
+            this.label_KhachHang.AutoSize = true;
+            this.label_KhachHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_KhachHang.Location = new System.Drawing.Point(3, 95);
+            this.label_KhachHang.Name = "label_KhachHang";
+            this.label_KhachHang.Size = new System.Drawing.Size(86, 18);
+            this.label_KhachHang.TabIndex = 9;
+            this.label_KhachHang.Text = "Khách hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(303, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "SĐT";
+            // 
+            // button_ThemDV
+            // 
+            this.button_ThemDV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_ThemDV.Location = new System.Drawing.Point(303, 129);
+            this.button_ThemDV.Name = "button_ThemDV";
+            this.button_ThemDV.Size = new System.Drawing.Size(94, 28);
+            this.button_ThemDV.TabIndex = 11;
+            this.button_ThemDV.Text = "Thêm";
+            this.button_ThemDV.UseVisualStyleBackColor = true;
+            // 
+            // button_SuaDV
+            // 
+            this.button_SuaDV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_SuaDV.Location = new System.Drawing.Point(403, 129);
+            this.button_SuaDV.Name = "button_SuaDV";
+            this.button_SuaDV.Size = new System.Drawing.Size(94, 28);
+            this.button_SuaDV.TabIndex = 12;
+            this.button_SuaDV.Text = "Sửa";
+            this.button_SuaDV.UseVisualStyleBackColor = true;
+            // 
+            // button_XoaDV
+            // 
+            this.button_XoaDV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_XoaDV.Location = new System.Drawing.Point(503, 129);
+            this.button_XoaDV.Name = "button_XoaDV";
+            this.button_XoaDV.Size = new System.Drawing.Size(94, 28);
+            this.button_XoaDV.TabIndex = 13;
+            this.button_XoaDV.Text = "Xóa";
+            this.button_XoaDV.UseVisualStyleBackColor = true;
+            // 
+            // button_Huy
+            // 
+            this.button_Huy.Location = new System.Drawing.Point(306, 338);
+            this.button_Huy.Margin = new System.Windows.Forms.Padding(15, 3, 3, 3);
+            this.button_Huy.Name = "button_Huy";
+            this.button_Huy.Size = new System.Drawing.Size(104, 26);
+            this.button_Huy.TabIndex = 14;
+            this.button_Huy.Text = "Hủy";
+            this.button_Huy.UseVisualStyleBackColor = true;
+            // 
+            // button_OK
+            // 
+            this.button_OK.AutoSize = true;
+            this.button_OK.Location = new System.Drawing.Point(193, 338);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(104, 26);
+            this.button_OK.TabIndex = 15;
+            this.button_OK.Text = "OK";
+            this.button_OK.UseVisualStyleBackColor = true;
+            // 
+            // label_CT_PhieuDV
+            // 
+            this.label_CT_PhieuDV.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label_CT_PhieuDV, 3);
+            this.label_CT_PhieuDV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_CT_PhieuDV.Location = new System.Drawing.Point(3, 132);
+            this.label_CT_PhieuDV.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
+            this.label_CT_PhieuDV.Name = "label_CT_PhieuDV";
+            this.label_CT_PhieuDV.Size = new System.Drawing.Size(225, 20);
+            this.label_CT_PhieuDV.TabIndex = 14;
+            this.label_CT_PhieuDV.Text = "Danh sách dịch vụ sử dụng";
+            // 
+            // textBox_NgayLap
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox_NgayLap, 2);
+            this.textBox_NgayLap.Location = new System.Drawing.Point(403, 61);
+            this.textBox_NgayLap.Name = "textBox_NgayLap";
+            this.textBox_NgayLap.Size = new System.Drawing.Size(194, 20);
+            this.textBox_NgayLap.TabIndex = 8;
+            // 
+            // PhieuDichVu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "PhieuDichVu";
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button_OK);
+            this.Controls.Add(this.button_Huy);
+            this.Name = "PhieuDichVu";
+            this.Text = "Nhập phiếu dịch vụ";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_NgayLap;
+        private System.Windows.Forms.TextBox textBox_SoPhieu;
+        private System.Windows.Forms.Label label_SoPhieu;
+        private System.Windows.Forms.Label label_KhachHang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_ThemDV;
+        private System.Windows.Forms.Button button_SuaDV;
+        private System.Windows.Forms.Button button_XoaDV;
+        private System.Windows.Forms.Button button_Huy;
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Label label_CT_PhieuDV;
+        private System.Windows.Forms.TextBox textBox_NgayLap;
     }
 }
