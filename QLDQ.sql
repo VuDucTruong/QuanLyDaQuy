f794c7c6ee271f61a2ad6889104a6e45721316b8
VuDucTruong <21520507@gm.uit.edu.vn>
2023-05-25 22:55:16 +0700

    update QLDQ.sql

--- QLDQ.sql
+++ QLDQ.sql
@@ -1,10 +1,10 @@
-create database QLDQ
+﻿create database QLDQ
 use QLDQ
 
 create table SANPHAM
 (
-	MaSP int identity(0,1) primary key,
-	TenSP nvarchar(100),
+	MaSP int identity(1,1) primary key,
+	TenSP nvarchar(100) unique,
 	MaLSP int ,
 	DonGiaBan float,
 	DonGiaMua float,
@@ -12,34 +12,34 @@ create table SANPHAM
 )
 create table DONVITINH
 (
-	MaDVT int identity(0,1) primary key,
-	DVT nvarchar(100)
+	MaDVT int identity(1,1) primary key,
+	DVT nvarchar(100) unique
 )
 create table LOAISANPHAM
 (
-	MaLSP int identity(0,1) primary key,
-	TenLSP nvarchar(100) , 
+	MaLSP int identity(1,1) primary key,
+	TenLSP nvarchar(100) unique, 
 	LoiNhuan float,
 	MaDVT int
 )
 
 create table KHACHHANG 
 (
-	MaKH int identity(0,1) primary key,
+	MaKH int identity(1,1) primary key,
 	TenKH nvarchar(100),
 	SDT varchar(20)
 )
 
 create table DICHVU
 (
-	MaDV int identity(0,1) primary key,
-	TenDV nvarchar(100),
+	MaDV int identity(1,1) primary key,
+	TenDV nvarchar(100) unique,
 	DonGiaDV float
 )
 
 create table NHACUNGCAP
 (
-	MaNCC int identity(0,1) primary key,
+	MaNCC int identity(1,1) primary key,
 	TenNCC nvarchar(100),
 	DiaChi nvarchar(100),
 	SDT varchar(20)
@@ -59,7 +59,7 @@ create table TONKHO
 
 create table PHIEUMUAHANG
 (
-	MaPhieuMH int identity(0,1) primary key,
+	MaPhieuMH int identity(1,1) primary key,
 	MaNCC int,
 	NgayLap smalldatetime,
 	TongTien float
@@ -76,7 +76,7 @@ create table CT_PHIEUMUAHANG
 )
 create table PHIEUBANHANG
 (
-	MaPhieuBH int identity(0,1) primary key,
+	MaPhieuBH int identity(1,1) primary key,
 	MaKH int,
 	NgayLap smalldatetime,
 	TongTien float
@@ -93,7 +93,7 @@ create table CT_PHIEUBANHANG
 )
 create table PHIEUDICHVU
 (
-	MaPhieuDV int identity(0,1) primary key,
+	MaPhieuDV int identity(1,1) primary key,
 	MaKH int,
 	NgayLap smalldatetime,
 	TongTien float,
@@ -119,7 +119,7 @@ create table CT_PHIEUDICHVU
 )
 create table THAMSO
 (
-	MaThamSo int identity(0,1) primary key,
+	MaThamSo int identity(1,1) primary key,
 	TenThamSo varchar(100) ,
 	GiaTri float
 )
@@ -137,6 +137,107 @@ alter table PhieuDichVu add constraint FK_PhieuDV_KH foreign key (MaKH) referenc
 alter table CT_PhieuDichVu add constraint FK_CTPDV_PDV foreign key (MaPhieuDV) references PhieuDichVu (MaPhieuDV)
 alter table CT_PhieuDichVu add constraint FK_CTPDV_DV foreign key (MaDV) references DichVu (MaDV)
 
+--reset identity---
+--DBCC CHECKIDENT ( DONVITINH , RESEED , 0 )
+--DBCC CHECKIDENT ( LOAISANPHAM , RESEED , 0)
+--DBCC CHECKIDENT (SANPHAM , RESEED , 0)
+--trigger---
+go
+create trigger trg_THEMSANPHAM
+on SANPHAM
+for insert , update
+as begin
+	declare @LOINHUAN float
+	declare @MaSP int
+	select @MaSP = MaSP from inserted
+	select @LOINHUAN = LoiNhuan from LOAISANPHAM , inserted where inserted.MaLSP = LOAISANPHAM.MaLSP
+	update SANPHAM
+	set DonGiaBan = DonGiaMua * ( 100 + @LOINHUAN ) /100
+	where MaSP = @MaSP
+end
+--select *
+select * from DONVITINH
+select * from LOAISANPHAM
+select * from SANPHAM
+delete from SANPHAM
+--DONVITINH
+insert into DONVITINH values (N'Cặp')
+insert into DONVITINH values (N'Chiếc')
+--LOAISANPHAM
+insert into LOAISANPHAM values 
+(N'Lắc' , 5 , 1), -- MALSP = 1
+(N'Vòng' , 5 , 1), -- 2
+(N'Dây cổ' , 5 , 1), -- 3
+(N'Dây chuyền', 5 , 1), -- 4
+(N'Kiềng' , 5 , 1 ), -- 5
+(N'Bông tai' , 5 , 2), -- 6
+(N'Nhẫn' , 5 , 1), -- 7
+(N'Mặt dây chuyền' , 5 , 1), -- 8
+(N'Đồng hồ' , 5 , 1), -- 9
+(N'Đồng hồ cặp' , 5 , 2) -- 10
+---DICHVU--
+insert into DICHVU values (N'Kiểm định chất lượng đá quý' , 200000)
+insert into DICHVU values (N'Kiểm định chất lượng vàng bạc' , 100000)
+insert into DICHVU values (N'Vệ sinh sản phẩm' , 100000)
+insert into DICHVU values (N'Sửa chữa' , 100000)
+insert into DICHVU values (N'Đánh bóng' , 100000)
+insert into DICHVU values (N'Khắc tên , họa tiết' , 200000)
+insert into DICHVU values (N'Trả/Đổi hàng' , 100000)
 
-
-
+---SANPHAM
+--Lắc--
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc tay bạc đính ngọc trai STYLE x CHOU CHOU NHNHY000001' , 1 , 910000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc tay bạc Ý 0000W000050' , 1 , 500000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc tay Bạc đính đá XMXMW000019', 1 , 1400000, 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc tay bạc Ý 0000W060094' , 1 , 1000000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc tay trẻ em bạc Disney hình chuột Mickey đen 0000Y060009' , 1 , 480000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc bạc DIY 0000W060029' , 1 , 350000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc bạc đính đá Disney Frozen NHMXW000002' , 1 , 1000000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc tay Bạc đính đá CZ XM00W000011' , 1 , 1800000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc tay Bạc đính đá XMXMW060061' , 1 , 800000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Lắc charm nam bạc My Man 0000K060015' , 1 , 500000 , 0)
+---Vòng---
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Vòng tay Vàng 18K đính đá Ruby RB00Y000008' , 2 , 9000000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Vòng tay Vàng trắng 14K đính đá Topaz TPXMW000021' , 2 , 41254000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Vòng tay Vàng 18K đính ngọc trai Freshwater PFXMY000010' , 2 , 27410000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Vòng tay Vàng trắng Ý 18K PNJ 0000W060385' , 2 , 21990000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Vòng tay Kim cương Vàng trắng 14K DDDDW060048' , 2 , 114500000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Vòng tay trẻ em Vàng 14K HELLO KITTY 0000C060046' , 2 , 6290000 , 0)
+--Dây cổ
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây cổ Bạc 0000Y060036' , 3 , 975000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây cổ Vàng Ý 18K 0000Z000035' , 3 , 39538000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây cổ Vàng 18K đính đá Citrine CTMXY000012' , 3 , 130134000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây cổ Vàng trắng 14K đính đá Sapphire SPMXW000001' , 3 , 143184000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây cổ Vàng 18K đính đá Ruby RBMXY000012' , 3 , 99682000, 0)
+--Dây chuyền
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây chuyền Vàng 18K 0000Y060475' , 4 , 19334000, 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây chuyền Vàng trắng Ý 18K 0000W060970' , 4 , 32590000, 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây chuyền nam Vàng Ý 18K 0000Y060509' , 4 , 41990000 , 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây chuyền nam Vàng trắng Ý 18K 0000W061215' , 4 , 11590000, 0)
+insert into SANPHAM ( TenSP , MaLSP , DonGiaMua , SoLuongTon) values
+(N'Dây chuyền Bạc 0000W000022' , 4 , 1295000, 0)
