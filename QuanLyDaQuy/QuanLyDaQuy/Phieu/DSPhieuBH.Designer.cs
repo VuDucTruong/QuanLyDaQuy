361a31a44fae8132ef3e03e833f30008e3393ccf
VuDucTruong <21520507@gm.uit.edu.vn>
2023-06-12 14:06:35 +0700

    changename

--- QuanLyDaQuy/QuanLyDaQuy/Phieu/DSPhieuBH.Designer.cs
+++ QuanLyDaQuy/QuanLyDaQuy/Phieu/DSPhieuBH.Designer.cs
@@ -1,6 +1,7 @@
-﻿namespace QuanLyDaQuy.Phieu
+﻿
+namespace QuanLyDaQuy.Phieu
 {
-    partial class DSPhieuBH
+    partial class DsPhieuBH
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +29,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbb_searchMode = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_keyWord = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.dgv_ds_pbh = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ds_pbh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dgv_ds_pbh, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(982, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(976, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh sách phiếu bán hàng";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.27049F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.64754F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.43443F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.913935F));
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cbb_searchMode, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tb_keyWord, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btn_search, 4, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 70);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(976, 39);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 5);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 34);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tìm kiếm theo:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // cbb_searchMode
+            // 
+            this.cbb_searchMode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbb_searchMode.Items.AddRange(new object[] {
+            "Tất cả",
+            "Mã phiếu",
+            "Mã khách hàng",
+            "Tên khách hàng",
+            "Số điện thoại",
+            "Ngày lập"});
+            this.cbb_searchMode.Location = new System.Drawing.Point(112, 3);
+            this.cbb_searchMode.Name = "cbb_searchMode";
+            this.cbb_searchMode.Size = new System.Drawing.Size(175, 24);
+            this.cbb_searchMode.TabIndex = 0;
+            this.cbb_searchMode.SelectedIndexChanged += new System.EventHandler(this.cbb_searchMode_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Location = new System.Drawing.Point(320, 5);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 34);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Từ khóa:";
+            // 
+            // tb_keyWord
+            // 
+            this.tb_keyWord.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tb_keyWord.Location = new System.Drawing.Point(390, 3);
+            this.tb_keyWord.Name = "tb_keyWord";
+            this.tb_keyWord.Size = new System.Drawing.Size(494, 22);
+            this.tb_keyWord.TabIndex = 2;
+            this.tb_keyWord.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_keyWord_KeyDown);
+            // 
+            // btn_search
+            // 
+            this.btn_search.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_search.Location = new System.Drawing.Point(890, 3);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(83, 23);
+            this.btn_search.TabIndex = 3;
+            this.btn_search.Text = "Tìm kiếm";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // dgv_ds_pbh
+            // 
+            this.dgv_ds_pbh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_ds_pbh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_ds_pbh.Location = new System.Drawing.Point(3, 115);
+            this.dgv_ds_pbh.Name = "dgv_ds_pbh";
+            this.dgv_ds_pbh.RowHeadersVisible = false;
+            this.dgv_ds_pbh.RowHeadersWidth = 51;
+            this.dgv_ds_pbh.RowTemplate.Height = 24;
+            this.dgv_ds_pbh.Size = new System.Drawing.Size(976, 332);
+            this.dgv_ds_pbh.TabIndex = 2;
+            // 
+            // DsPhieuBH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DSPhieuBH";
+            this.ClientSize = new System.Drawing.Size(982, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "DsPhieuBH";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Phiếu";
+            this.Load += new System.EventHandler(this.DsPhieuBH_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ds_pbh)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbb_searchMode;
+        private System.Windows.Forms.TextBox tb_keyWord;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.DataGridView dgv_ds_pbh;
     }
 }
--- QuanLyDaQuy/QuanLyDaQuy/Phieu/DSPhieuBH.cs
+++ QuanLyDaQuy/QuanLyDaQuy/Phieu/DSPhieuBH.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using QuanLyDaQuy.DAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +12,151 @@ using System.Windows.Forms;
 
 namespace QuanLyDaQuy.Phieu
 {
-    public partial class DSPhieuBH : Form
+    public partial class DsPhieuBH : Form
     {
-        public DSPhieuBH()
+        public DsPhieuBH()
         {
             InitializeComponent();
+        }
+
+        private void DsPhieuBH_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cbb_searchMode.SelectedIndex = 0;
+
+                string query = "SELECT PBH.MaPhieuBH, PBH.MaKH, KH.TenKH, KH.SDT, PBH.NgayLap, PBH.TongTien " +
+                               "FROM PHIEUBANHANG PBH " +
+                               "JOIN KHACHHANG KH ON PBH.MaKH = KH.MaKH";
+
+                DataTable dataTable = DataProvider.Instance.ExecuteQuery(query);
+
+                dgv_ds_pbh.DataSource = dataTable;
+
+                // Gọi hàm AddRowNumbers để thêm cột STT vào DataTable
+                //AddRowNumbers(dataTable, "STT");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void AddRowNumbers(DataTable dataTable, string columnName)
+        {
+            // Thêm cột số thứ tự vào DataTable
+            dataTable.Columns.Add(columnName, typeof(int));
+
+            // Đánh số thứ tự cho mỗi dòng trong DataTable
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                dataTable.Rows[i][columnName] = i + 1;
+            }
+
+            dgv_ds_pbh.DataSource = dataTable;
+            dgv_ds_pbh.Columns["STT"].Width = 50;
+            dgv_ds_pbh.Columns["STT"].DisplayIndex = 0;
+
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int searchOption = cbb_searchMode.SelectedIndex;
+                string keyword = tb_keyWord.Text.Trim();
+
+                // Kiểm tra từ khóa tìm kiếm không được rỗng 
+                if (cbb_searchMode.SelectedIndex != 0 && string.IsNullOrEmpty(keyword))
+                {
+                    MessageBox.Show("Vui lòng nhập từ khóa tìm kiếm!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                string query = "";
+                switch (searchOption)
+                {
+                    case 0: // Tất cả
+                        query = "SELECT PBH.MaPhieuBH, PBH.MaKH, KH.TenKH, KH.SDT, PBH.NgayLap, PBH.TongTien " +
+                                       "FROM PHIEUBANHANG PBH " +
+                                       "JOIN KHACHHANG KH ON PBH.MaKH = KH.MaKH";
+                        break;
+
+                    case 1: // Mã phiếu
+                        query = "SELECT p.MaPhieuBH, p.MaKH, k.TenKH, k.SDT, p.NgayLap, p.TongTien FROM PHIEUBANHANG p INNER JOIN KHACHHANG k ON p.MaKH = k.MaKH WHERE p.MaPhieuBH = @Keyword";
+                        break;
+                    case 2: // Mã khách hàng
+                        query = "SELECT p.MaPhieuBH, p.MaKH, k.TenKH, k.SDT, p.NgayLap, p.TongTien FROM PHIEUBANHANG p INNER JOIN KHACHHANG k ON p.MaKH = k.MaKH WHERE p.MaKH = @Keyword";
+                        break;
+                    case 3: // Tên khách hàng
+                        query = "SELECT p.MaPhieuBH, p.MaKH, k.TenKH, k.SDT, p.NgayLap, p.TongTien FROM PHIEUBANHANG p INNER JOIN KHACHHANG k ON p.MaKH = k.MaKH WHERE k.TenKH LIKE '%' + @Keyword + '%'";
+                        break;
+                    case 4: // Số điện thoại
+                        query = "SELECT p.MaPhieuBH, p.MaKH, k.TenKH, k.SDT, p.NgayLap, p.TongTien FROM PHIEUBANHANG p INNER JOIN KHACHHANG k ON p.MaKH = k.MaKH WHERE k.SDT LIKE '%' + @Keyword + '%'";
+                        break;
+                    case 5: // Ngày lập
+                        {
+                            string format = "d/M/yyyy";
+                            DateTime date;
+
+                            if (!DateTime.TryParseExact(keyword, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
+                            {
+                                MessageBox.Show("Vui lòng nhập theo định dạng ngày/tháng/năm", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                return;
+                            }
+
+                            query = "SELECT p.MaPhieuBH, p.MaKH, k.TenKH, k.SDT, p.NgayLap, p.TongTien " +
+                            "FROM PHIEUBANHANG p " +
+                            "INNER JOIN KHACHHANG k ON p.MaKH = k.MaKH " +
+                            "WHERE CONVERT(date, p.NgayLap) = @Keyword ";
+
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+
+                DataTable dataTable = DataProvider.Instance.ExecuteQuery(query, new object[] { keyword });
+                if (dataTable.Rows.Count > 0)
+                {
+                    dgv_ds_pbh.DataSource = dataTable;
+
+                    // Gọi hàm AddRowNumbers để thêm cột STT vào DataTable
+                    //AddRowNumbers(dataTable, "STT");
+                }
+                else
+                {
+                    dgv_ds_pbh.DataSource = null;
+                    MessageBox.Show("Không tìm thấy kết quả!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi khi tìm kiếm: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cbb_searchMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbb_searchMode.SelectedIndex == 0) // Chọn option "Tất cả"
+                tb_keyWord.Enabled = false;
+            else
+                tb_keyWord.Enabled = true;
+
+            tb_keyWord.Text = "";
+
+        }
+
+        private void tb_keyWord_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_search.PerformClick();
+
+                // Ngăn chặn âm thanh "beep"
+                e.SuppressKeyPress = true;
+            }
         }
     }
 }
