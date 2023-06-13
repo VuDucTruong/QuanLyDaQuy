cc1e5cc6f7e8ddf508be929f6b43293e026ce918
Lê Khai Trí <lekhaitri.2003@gmail.com>
2023-06-13 22:41:49 +0700

    add ThayDoiThamSo form

---
+++ QuanLyDaQuy/QuanLyDaQuy/Phieu/ThayDoiThamSo.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace QuanLyDaQuy.Phieu
+{
+    partial class ThayDoiThamSo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_ds_thamSo = new System.Windows.Forms.DataGridView();
+            this.maTS_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tenTS_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.giatri_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ds_thamSo)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv_ds_thamSo
+            // 
+            this.dgv_ds_thamSo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_ds_thamSo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.maTS_col,
+            this.tenTS_col,
+            this.giatri_col});
+            this.dgv_ds_thamSo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_ds_thamSo.Location = new System.Drawing.Point(3, 42);
+            this.dgv_ds_thamSo.Name = "dgv_ds_thamSo";
+            this.dgv_ds_thamSo.RowHeadersWidth = 51;
+            this.dgv_ds_thamSo.RowTemplate.Height = 24;
+            this.dgv_ds_thamSo.Size = new System.Drawing.Size(636, 178);
+            this.dgv_ds_thamSo.TabIndex = 0;
+            // 
+            // maTS_col
+            // 
+            this.maTS_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.maTS_col.HeaderText = "Mã tham số";
+            this.maTS_col.MinimumWidth = 6;
+            this.maTS_col.Name = "maTS_col";
+            this.maTS_col.ReadOnly = true;
+            this.maTS_col.Width = 110;
+            // 
+            // tenTS_col
+            // 
+            this.tenTS_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.tenTS_col.HeaderText = "Tên tham số";
+            this.tenTS_col.MinimumWidth = 6;
+            this.tenTS_col.Name = "tenTS_col";
+            this.tenTS_col.ReadOnly = true;
+            // 
+            // giatri_col
+            // 
+            this.giatri_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.giatri_col.HeaderText = "Giá trị";
+            this.giatri_col.MinimumWidth = 6;
+            this.giatri_col.Name = "giatri_col";
+            this.giatri_col.Width = 75;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dgv_ds_thamSo, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(642, 263);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.81132F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.2956F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.72142F));
+            this.tableLayoutPanel2.Controls.Add(this.btn_save, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btn_cancel, 2, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 226);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(636, 34);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(636, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "THAY ĐỔI THAM SỐ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btn_save.Location = new System.Drawing.Point(447, 3);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(94, 28);
+            this.btn_save.TabIndex = 0;
+            this.btn_save.Text = "Lưu thay đổi";
+            this.btn_save.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(557, 3);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 28);
+            this.btn_cancel.TabIndex = 1;
+            this.btn_cancel.Text = "Hủy";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            // 
+            // ThayDoiThamSo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 263);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ThayDoiThamSo";
+            this.Text = "ThayDoiThamSo";
+            this.Load += new System.EventHandler(this.ThayDoiThamSo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ds_thamSo)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_ds_thamSo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maTS_col;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tenTS_col;
+        private System.Windows.Forms.DataGridViewTextBoxColumn giatri_col;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ QuanLyDaQuy/QuanLyDaQuy/Phieu/ThayDoiThamSo.cs
@@ -0,0 +1,37 @@
+﻿using QuanLyDaQuy.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyDaQuy.Phieu
+{
+    public partial class ThayDoiThamSo : Form
+    {
+        public ThayDoiThamSo()
+        {
+            InitializeComponent();
+        }
+
+        private void ThayDoiThamSo_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "SELECT MaThamSo, TenThamSo, GiaTri FROM THAMSO";
+
+                DataTable thamSoData = DataProvider.Instance.ExecuteQuery(query);
+                dgv_ds_thamSo.DataSource = thamSoData;
+
+            }
+            catch
+            {
+                MessageBox.Show("Lỗi load dữ liệu bảng tham số");
+            }
+        }
+    }
+}
