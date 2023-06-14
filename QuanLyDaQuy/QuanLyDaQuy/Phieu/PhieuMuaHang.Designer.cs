9d9f13b9f1589685ccc3c44d7323881a1a0997fa
Lê Khai Trí <lekhaitri.2003@gmail.com>
2023-06-14 14:06:52 +0700

    enable pmh

--- QuanLyDaQuy/QuanLyDaQuy/Phieu/PhieuMuaHang.Designer.cs
+++ QuanLyDaQuy/QuanLyDaQuy/Phieu/PhieuMuaHang.Designer.cs
@@ -250,7 +250,6 @@
             this.dt_grid_phieumuahang.GridColor = System.Drawing.SystemColors.Control;
             this.dt_grid_phieumuahang.Location = new System.Drawing.Point(3, 143);
             this.dt_grid_phieumuahang.Name = "dt_grid_phieumuahang";
-            this.dt_grid_phieumuahang.ReadOnly = true;
             this.dt_grid_phieumuahang.RowHeadersWidth = 51;
             this.dt_grid_phieumuahang.RowTemplate.Height = 24;
             this.dt_grid_phieumuahang.Size = new System.Drawing.Size(967, 337);
@@ -268,7 +267,6 @@
             this.phieuMuaHang_column_STT.HeaderText = "STT";
             this.phieuMuaHang_column_STT.MinimumWidth = 6;
             this.phieuMuaHang_column_STT.Name = "phieuMuaHang_column_STT";
-            this.phieuMuaHang_column_STT.ReadOnly = true;
             this.phieuMuaHang_column_STT.Width = 64;
             // 
             // phieuMuaHang_column_sanPham
@@ -277,7 +275,6 @@
             this.phieuMuaHang_column_sanPham.HeaderText = "Sản phẩm";
             this.phieuMuaHang_column_sanPham.MinimumWidth = 6;
             this.phieuMuaHang_column_sanPham.Name = "phieuMuaHang_column_sanPham";
-            this.phieuMuaHang_column_sanPham.ReadOnly = true;
             this.phieuMuaHang_column_sanPham.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.phieuMuaHang_column_sanPham.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
@@ -287,7 +284,6 @@
             this.phieuMuaHang_column_loaiSanPham.HeaderText = "Loại sản phẩm";
             this.phieuMuaHang_column_loaiSanPham.MinimumWidth = 6;
             this.phieuMuaHang_column_loaiSanPham.Name = "phieuMuaHang_column_loaiSanPham";
-            this.phieuMuaHang_column_loaiSanPham.ReadOnly = true;
             this.phieuMuaHang_column_loaiSanPham.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.phieuMuaHang_column_loaiSanPham.Width = 119;
             // 
@@ -297,7 +293,6 @@
             this.phieuMuaHang_column_soLuong.HeaderText = "Số lượng";
             this.phieuMuaHang_column_soLuong.MinimumWidth = 6;
             this.phieuMuaHang_column_soLuong.Name = "phieuMuaHang_column_soLuong";
-            this.phieuMuaHang_column_soLuong.ReadOnly = true;
             this.phieuMuaHang_column_soLuong.Width = 86;
             // 
             // phieuMuaHang_column_donViTinh
@@ -306,7 +301,6 @@
             this.phieuMuaHang_column_donViTinh.HeaderText = "Đơn vị tính";
             this.phieuMuaHang_column_donViTinh.MinimumWidth = 6;
             this.phieuMuaHang_column_donViTinh.Name = "phieuMuaHang_column_donViTinh";
-            this.phieuMuaHang_column_donViTinh.ReadOnly = true;
             this.phieuMuaHang_column_donViTinh.Width = 75;
             // 
             // phieuMuaHang_column_donGia
@@ -315,7 +309,6 @@
             this.phieuMuaHang_column_donGia.HeaderText = "Đơn giá";
             this.phieuMuaHang_column_donGia.MinimumWidth = 6;
             this.phieuMuaHang_column_donGia.Name = "phieuMuaHang_column_donGia";
-            this.phieuMuaHang_column_donGia.ReadOnly = true;
             this.phieuMuaHang_column_donGia.Width = 63;
             // 
             // phieuMuaHang_column_thanhTien
@@ -324,7 +317,6 @@
             this.phieuMuaHang_column_thanhTien.HeaderText = "Thành tiền";
             this.phieuMuaHang_column_thanhTien.MinimumWidth = 6;
             this.phieuMuaHang_column_thanhTien.Name = "phieuMuaHang_column_thanhTien";
-            this.phieuMuaHang_column_thanhTien.ReadOnly = true;
             this.phieuMuaHang_column_thanhTien.Width = 97;
             // 
             // lb_thanhTien
