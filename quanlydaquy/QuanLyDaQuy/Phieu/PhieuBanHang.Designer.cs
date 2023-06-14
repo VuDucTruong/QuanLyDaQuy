a4c6e14d7ffd7b4995bd3e96e4d290fb233d0fb3
leehungw <hungttalop83@gmail.com>
2023-06-14 14:36:06 +0700

    Update STT phieu ban hang

--- quanlydaquy/QuanLyDaQuy/Phieu/PhieuBanHang.Designer.cs
+++ quanlydaquy/QuanLyDaQuy/Phieu/PhieuBanHang.Designer.cs
@@ -29,22 +29,15 @@ namespace QuanLyDaQuy.Phieu
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.lb_title = new System.Windows.Forms.Label();
             this.lb_sophieu = new System.Windows.Forms.Label();
             this.lb_khachhang = new System.Windows.Forms.Label();
             this.lb_ngaylap = new System.Windows.Forms.Label();
             this.dgv_phieubanhang = new System.Windows.Forms.DataGridView();
-            this.stt_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.sp_col = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.lsp_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.sl_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dvt_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dg_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tt_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tb_sophieu = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
@@ -60,6 +53,13 @@ namespace QuanLyDaQuy.Phieu
             this.btn_ok = new System.Windows.Forms.Button();
             this.btn_XuatPDF = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.stt_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sp_col = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.lsp_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sl_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dvt_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dg_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tt_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_phieubanhang)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -119,14 +119,14 @@ namespace QuanLyDaQuy.Phieu
             // dgv_phieubanhang
             // 
             this.dgv_phieubanhang.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgv_phieubanhang.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_phieubanhang.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgv_phieubanhang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv_phieubanhang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.stt_col,
@@ -136,27 +136,27 @@ namespace QuanLyDaQuy.Phieu
             this.dvt_col,
             this.dg_col,
             this.tt_col});
-            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle11.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle11.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgv_phieubanhang.DefaultCellStyle = dataGridViewCellStyle11;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv_phieubanhang.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgv_phieubanhang.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgv_phieubanhang.GridColor = System.Drawing.Color.Silver;
             this.dgv_phieubanhang.Location = new System.Drawing.Point(3, 183);
             this.dgv_phieubanhang.MultiSelect = false;
             this.dgv_phieubanhang.Name = "dgv_phieubanhang";
-            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle12.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle12.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle12.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgv_phieubanhang.RowHeadersDefaultCellStyle = dataGridViewCellStyle12;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_phieubanhang.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dgv_phieubanhang.RowHeadersWidth = 51;
             this.dgv_phieubanhang.RowTemplate.Height = 24;
             this.dgv_phieubanhang.Size = new System.Drawing.Size(976, 324);
@@ -166,77 +166,6 @@ namespace QuanLyDaQuy.Phieu
             this.dgv_phieubanhang.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dgv_phieubanhang_EditingControlShowing);
             this.dgv_phieubanhang.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dgv_phieubanhang_RowsAdded);
             this.dgv_phieubanhang.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.dgv_phieubanhang_RowsRemoved);
-            // 
-            // stt_col
-            // 
-            this.stt_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.stt_col.HeaderText = "STT";
-            this.stt_col.MinimumWidth = 6;
-            this.stt_col.Name = "stt_col";
-            this.stt_col.Width = 63;
-            // 
-            // sp_col
-            // 
-            this.sp_col.AutoComplete = false;
-            this.sp_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.sp_col.DataPropertyName = "TenSP";
-            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.sp_col.DefaultCellStyle = dataGridViewCellStyle10;
-            this.sp_col.DropDownWidth = 2;
-            this.sp_col.HeaderText = "Sản phẩm";
-            this.sp_col.MinimumWidth = 6;
-            this.sp_col.Name = "sp_col";
-            this.sp_col.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.sp_col.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // lsp_col
-            // 
-            this.lsp_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.lsp_col.DataPropertyName = "TenLSP";
-            this.lsp_col.HeaderText = "Loại sản phẩm";
-            this.lsp_col.MinimumWidth = 6;
-            this.lsp_col.Name = "lsp_col";
-            this.lsp_col.ReadOnly = true;
-            this.lsp_col.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.lsp_col.Width = 124;
-            // 
-            // sl_col
-            // 
-            this.sl_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.sl_col.DataPropertyName = "SoLuongTon";
-            this.sl_col.HeaderText = "Số lượng";
-            this.sl_col.MinimumWidth = 6;
-            this.sl_col.Name = "sl_col";
-            this.sl_col.Width = 89;
-            // 
-            // dvt_col
-            // 
-            this.dvt_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.dvt_col.DataPropertyName = "DVT";
-            this.dvt_col.HeaderText = "Đơn vị tính";
-            this.dvt_col.MinimumWidth = 6;
-            this.dvt_col.Name = "dvt_col";
-            this.dvt_col.ReadOnly = true;
-            this.dvt_col.Width = 96;
-            // 
-            // dg_col
-            // 
-            this.dg_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.dg_col.DataPropertyName = "DonGiaBan";
-            this.dg_col.HeaderText = "Đơn giá";
-            this.dg_col.MinimumWidth = 6;
-            this.dg_col.Name = "dg_col";
-            this.dg_col.ReadOnly = true;
-            this.dg_col.Width = 82;
-            // 
-            // tt_col
-            // 
-            this.tt_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.tt_col.HeaderText = "Thành tiền";
-            this.tt_col.MinimumWidth = 6;
-            this.tt_col.Name = "tt_col";
-            this.tt_col.ReadOnly = true;
-            this.tt_col.Width = 98;
             // 
             // tb_sophieu
             // 
@@ -451,6 +380,78 @@ namespace QuanLyDaQuy.Phieu
             this.label2.TabIndex = 19;
             this.label2.Text = "Tổng tiền:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // stt_col
+            // 
+            this.stt_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.stt_col.HeaderText = "STT";
+            this.stt_col.MinimumWidth = 6;
+            this.stt_col.Name = "stt_col";
+            this.stt_col.ReadOnly = true;
+            this.stt_col.Width = 63;
+            // 
+            // sp_col
+            // 
+            this.sp_col.AutoComplete = false;
+            this.sp_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.sp_col.DataPropertyName = "TenSP";
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.sp_col.DefaultCellStyle = dataGridViewCellStyle2;
+            this.sp_col.DropDownWidth = 2;
+            this.sp_col.HeaderText = "Sản phẩm";
+            this.sp_col.MinimumWidth = 6;
+            this.sp_col.Name = "sp_col";
+            this.sp_col.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.sp_col.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // lsp_col
+            // 
+            this.lsp_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.lsp_col.DataPropertyName = "TenLSP";
+            this.lsp_col.HeaderText = "Loại sản phẩm";
+            this.lsp_col.MinimumWidth = 6;
+            this.lsp_col.Name = "lsp_col";
+            this.lsp_col.ReadOnly = true;
+            this.lsp_col.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.lsp_col.Width = 124;
+            // 
+            // sl_col
+            // 
+            this.sl_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.sl_col.DataPropertyName = "SoLuongTon";
+            this.sl_col.HeaderText = "Số lượng";
+            this.sl_col.MinimumWidth = 6;
+            this.sl_col.Name = "sl_col";
+            this.sl_col.Width = 89;
+            // 
+            // dvt_col
+            // 
+            this.dvt_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dvt_col.DataPropertyName = "DVT";
+            this.dvt_col.HeaderText = "Đơn vị tính";
+            this.dvt_col.MinimumWidth = 6;
+            this.dvt_col.Name = "dvt_col";
+            this.dvt_col.ReadOnly = true;
+            this.dvt_col.Width = 96;
+            // 
+            // dg_col
+            // 
+            this.dg_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dg_col.DataPropertyName = "DonGiaBan";
+            this.dg_col.HeaderText = "Đơn giá";
+            this.dg_col.MinimumWidth = 6;
+            this.dg_col.Name = "dg_col";
+            this.dg_col.ReadOnly = true;
+            this.dg_col.Width = 82;
+            // 
+            // tt_col
+            // 
+            this.tt_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.tt_col.HeaderText = "Thành tiền";
+            this.tt_col.MinimumWidth = 6;
+            this.tt_col.Name = "tt_col";
+            this.tt_col.ReadOnly = true;
+            this.tt_col.Width = 98;
             // 
             // PhieuBanHang
             // 
